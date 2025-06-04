cd335f841487e9b792bb748dc8c2a163bbbe2302
Christo Pananjickal Baby <cpananjickalbab9796@conestogac.on.ca>
2025-06-04 03:02:45 -0400

    Update: Added a comment for connection string

--- utils/constants.py
+++ utils/constants.py
@@ -1,1 +1,3 @@
+# The connection string is normally stored in a separate file and add it to .gitignore for security reasons.
+# For this lab 3, to keep it simple, I have defined it here.
 CONNECTION_STRING = "postgresql://neondb_owner:npg_7gTj6wozXMAm@ep-tight-waterfall-a882ef8l-pooler.eastus2.azure.neon.tech/neondb?sslmode=require"
